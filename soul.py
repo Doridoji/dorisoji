86ef8a3f7d207c563be4f066f7ea0e48221175b4
Doridoji <112374528+Doridoji@users.noreply.github.com>
2025-03-23 15:33:08 +0530

    Add files via upload

---
+++ soul.py
@@ -0,0 +1,20 @@
+import requests
+url = "https://t.me/EngineMod"
+
+data = {
+    "problem": "This channel https://t.me/EngineModis running illegal activities.\n"
+    "It takes money from people to teach DDoS attacks on BGMI, hacking courses,\n"
+    "and attacks on Indian government sites. It also performs DDoS attacks on Telegram VCs.\n"
+    "Now, the channel owner has cleared history, so please review its past week's logs.",
+    "full_name": "@RJ_Crow",
+    "email": "amanmanna198-2@gmail.com",
+    "phone_number": "+918119878208",
+}
+response = requests.post(url, data=data)
+
+
+if response.status_code == 200:
+    print("Report submitted successfully!")
+else:
+    print(f"Failed to submit report. Status code: {response.status_code}")
+# file owner @SOULCRACK
